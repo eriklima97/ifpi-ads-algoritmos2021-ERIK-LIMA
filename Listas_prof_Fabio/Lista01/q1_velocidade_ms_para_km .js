0cf649834ce7119625b5e1af049fde6a653177ff
eriklima97 <erikthiagodesouzalima@gmail.com>
2021-06-01 22:10:06 -0300

    Revert "summary"
    This reverts commit 8c78a29a895c26c25fb488f39c6e09b2d3ccdf19.

--- Listas_prof_Fabio/Lista01/q1_velocidade_ms_para_km .js
+++ Listas_prof_Fabio/Lista01/q1_velocidade_ms_para_km .js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')()
+const input = require('prompt-sync')()
 
 // Entrada
 const velocidade_ms = Number(prompt('Velocidade (m/s)'))
@@ -6,7 +6,10 @@ console.log(' digite o valor do raio de uma esfera e veja o volume dela')
 const r = Number(input( ' digite o valor do raio: '))
 
 // Processamento
-const velocidade_km = velocidade_ms * 3.6
+const pi = 3.14
+const r3 = Math.pow (r , 3 )
+const v =  4 * pi * r3 / 3
 
 // Saída
 console.log('A velocidade', velocidade_ms,'ms', 'é igual a', velocidade_km, 'km')// Saída 
+console.log(' o volume dessa esfera é:', v.toFixed(2))
